545a0fd7296429ef06b6350ab654ee1f14a55332
Juncoder171299 <longtad1929@gmail.com>
2025-07-01 13:34:22 +0700

    baitapthem

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function sum() {
         i++;
     }
     console.log("tong cac so tu 1-100 = ", total);
+    alert("Tổng các số từ 1-100: " + total);
 }
 function checkPass() {
     do {
@@ -42,12 +43,14 @@ function checkPass() {
             alert("Bạn đã hủy đăng nhập.");
             break;
         }
-    } while (password != 123456) {
         if (password === "123456") {
             alert("Đăng nhập thành công!");
+            break;
+        } else {
+            alert("Mật khẩu sai. Vui lòng thử lại!");
         }
-    }
 
+    } while (true);
 }
 function math() {
     let a = 10;
@@ -57,13 +60,50 @@ function math() {
     console.log("tich a*b=", a * b);
     console.log("thuong a/b=", a / b);
     console.log("chia lay du a,b=", a % b);
-    console.log(a==b);
-    console.log(a===b);
-    console.log(a!=b);
-    console.log(a!==b);
-    console.log(a<b);
-    console.log(a>b);
-    console.log(a<=b);
-    console.log(a>=b);
+    console.log(a == b);
+    console.log(a === b);
+    console.log(a != b);
+    console.log(a !== b);
+    console.log(a < b);
+    console.log(a > b);
+    console.log(a <= b);
+    console.log(a >= b);
 
 }
+function oddORevenNumber() {
+    Inputnumber = prompt(" Input your number:");
+    switch (Inputnumber % 2) {
+        case 1:
+            console.log("Là số lẻ");
+            alert("Là số lẻ");
+            break;
+        case 0:
+            console.log("Là số chẵn");
+            alert("Là số chẵn");
+            break;
+        default:
+            console.log("Không hợp lệ");
+            alert("Không hợp lệ");
+    }
+}
+function CompetencyAssessment() {
+    score = prompt("Input your Score: ");
+    if (score == null) {
+        console.log("Không hợp lệ");
+        alert("Hủy");
+    }
+    else if (score >= 90) {
+        console.log("Giỏi");
+        alert("Giỏi");
+    } else if (score >= 70 && score < 90) {
+        console.log("Khá");
+        alert("Khá");
+    } else if (score >= 50 && score < 70) {
+        console.log("Trung bình");
+        alert("Trung bình");
+    } else {
+        console.log("Yếu");
+        alert("Yếu");
+    }
+}
+ 
